81bb870874fab41f5f568e639543f7ee01b1e541
YY-ops <907479349@qq.com>
2020-12-16 11:48:58 +0800

    二

--- vueapp/src/components/HelloWorld.vue
+++ vueapp/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h1>这是一个新项目</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
